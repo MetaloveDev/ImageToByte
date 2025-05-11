43f2d9d9f580c143047d9b69c7da7a97e2cb75f7
Maxum Neroslyk <maxum_neroslyk@ukr.net>
2025-05-11 12:57:40 -0700

    Init commit

---
+++ ImageToByte/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] files = Directory.GetFiles(@"../Images/");
+for (int i = 0; i < files.Length; i++) {
+    string file = files[i];
+    File.WriteAllBytes(@"../Documents/" + Path.GetFileNameWithoutExtension(file) + ".txt", File.ReadAllBytes(file));
+    Console.WriteLine(MathF.Ceiling(i / (float)files.Length * 100f) + "%");
+}
+Console.WriteLine("Done!");
